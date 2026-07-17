1a5adbfc140fd7363d2f9347e2e6b98d8ea66a09
MarcalCrdz <pedrocontraomundo777@gmail.com>
2026-07-17 10:55:45 -0300

    primeiro push teste

---
+++ src/main/java/com/marcal/drone_delivery_simulator/DroneDeliverySimulatorApplication.java
@@ -0,0 +1,13 @@
+package com.marcal.drone_delivery_simulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DroneDeliverySimulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DroneDeliverySimulatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marcal/drone_delivery_simulator/DroneDeliverySimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcal.drone_delivery_simulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DroneDeliverySimulatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
